68b61941af31f2e43f4051414488e8c6797f2c78
renatowow14 <renatogomes256@gmail.com>
2020-09-19 15:15:25 -0300

    update bug-fix

--- pipeline-ci.groovy
+++ pipeline-ci.groovy
@@ -2,7 +2,7 @@
 pipeline {
     //Agent é o NÓ que vai rodar o job
     agent any
- 
+
     //Fases do pipeline
     stages {
         stage('Build') {
@@ -10,37 +10,30 @@ pipeline {
                 echo 'Fazendo a build do projeto'
             }
         }
- 
-        parallel {
-            stage('Teste unitários') {
-                agent {
-                    agent any
+
+        stage('Continuous Delivery') {
+            parallel {
+                stage('Teste unitários') {
+                    steps {
+                        echo "Trecho 1"
+                    }
                 }
-                steps {
-                    echo "Trecho 1"
+
+                stage('Testes de aceitação') {
+                    steps {
+                        echo "Trecho 2"
+                    }
                 }
+
+                stage('Testes de negócio') {
+                    steps {
+                        echo "Trecho 3"
+                    }
+                }
+
             }
- 
-            stage('Testes de aceitação') {
-                agent {
-                    agent any
-                }
-                steps {
-                    echo "Trecho 2"
-                }
-            }
- 
-            stage('Testes de negócio') {
-                agent {
-                    agent any
-                }
-                steps {
-                    echo "Trecho 3"
-                }
-            }
- 
         }
- 
+
         stage('Deploy to Stage') {
             steps {
                 echo 'Fazer deploy em ambiente de homologação (staging).'
